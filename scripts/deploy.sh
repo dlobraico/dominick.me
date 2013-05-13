9dd334858867352e5345ecdc0772a5e27aaa5ba6
D. LoBraico <dj@lobraico.com>
2013-05-13 02:41:39 -0500

    updates deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,10 +4,12 @@ echo "deploying dominick.me"
 cd /home/deployer/apps/personal
 git pull git@git.pyg8.com:dominick.me.git master
 echo "recompiling scss"
+source /etc/profile.d/rbenv.sh
 sass --update tmpl/sass:public/css
 echo "stopping server"
 pkill -9 dal.native
 echo "rebuilding"
+source /etc/profile.d/opam.sh
 ocamlbuild -use-ocamlfind src/dal.native
 echo "starting server"
 nohup /home/deployer/apps/personal/dal.native -daemonize -environment production
